14946aba7c1d51fb77f964d43b67652b95f2ff34
William <william.marback@hotmail.com>
2020-11-11 10:09:40 +0100

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace bricks_novemberProjekt
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
