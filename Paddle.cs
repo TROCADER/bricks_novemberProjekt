88fb155c3721bb4666e95a76f66269c902452623
William <william.marback@hotmail.com>
2020-11-11 11:04:18 +0100

    Added comments for some of the code

--- Paddle.cs
+++ Paddle.cs
@@ -5,12 +5,15 @@ namespace bricks_novemberProjekt
 {
     public class Paddle
     {
+        // Ställer in storleken på paddlen
         public int xRec = 150;
         public int yRec = 20;
 
+        // Variabler för paddelns position, byts ut av klassen Window som ställer in paddlens position
         public float xPos = 0;
         public float yPos = 0;
 
+        // De två tangenterna för att kunna styra paddlen
         public KeyboardKey leftKey;
         public KeyboardKey rightKey;
 
@@ -22,6 +25,7 @@ namespace bricks_novemberProjekt
             this.rightKey = downKey;
         }
 
+        // Enkel metod för att rita ut paddlen
         public void Draw()
         {
             Raylib.DrawRectangle((int)xPos, (int)yPos, xRec, yRec, Color.WHITE);
--- Window.cs
+++ Window.cs
@@ -5,9 +5,11 @@ namespace bricks_novemberProjekt
 {
     public class Window
     {
+        // Ställer in storleken av fönstret
         private int xWindowSize = 1280;
         private int yWindowSize = 960;
 
+        // Ställer in hastigheten av paddlen och bollen
         private float paddleSpeed = 10f;
         private float ballSpeed;
 
@@ -16,11 +18,17 @@ namespace bricks_novemberProjekt
             Paddle paddle = new Paddle(xWindowSize/2, yWindowSize-30, KeyboardKey.KEY_LEFT, KeyboardKey.KEY_RIGHT);
             Ball ball = new Ball();
 
+            // Initierar fönstret samt begränsar FPS'en till 60 på grund av varierande FPS
+            // --> programmet körs snabbare eller långsammare beroende på datorn kapabilitet
+            // --> kan leda till en försämrad spelupplevelse, samt svårare att koda efter
             Raylib.InitWindow(xWindowSize, yWindowSize, "Bricks");
             Raylib.SetTargetFPS(60);
 
+            // Så länge fönstret är öppet så kommer programmet att loopas varje ny bild/frame
             while (!Raylib.WindowShouldClose())
             {
+                // Styrning av paddlen
+                // Ändrar x postion i paddelns klass
                 if (Raylib.IsKeyDown(paddle.leftKey))
                 {
                     paddle.xPos -= paddleSpeed;
@@ -30,7 +38,9 @@ namespace bricks_novemberProjekt
                 {
                     paddle.xPos += paddleSpeed;
                 }
-
+                
+                // Begränsar så att paddeln inte kan åka utanför bilden
+                // Kanske byter ut så att paddlen istället teleporterar till motsatta sidan
                 if (paddle.xPos == 0+20)
                 {
                     paddle.xPos += paddleSpeed;
@@ -43,6 +53,7 @@ namespace bricks_novemberProjekt
 
                 Raylib.BeginDrawing();
 
+                // Sätter bakgrunden till svart då paddlen, bollen samt varje tegelsten/brick kommer vara vit
                 Raylib.ClearBackground(Color.BLACK);
 
                 paddle.Draw();
